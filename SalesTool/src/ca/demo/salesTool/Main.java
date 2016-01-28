e6b0f8339227c7c22fbab9d8b17aebb637800a35
ylachraf <lachrafyounes@gmail.com>
2016-01-28 11:56:46 +0100

    Initial checkin of sales tool.

---
+++ SalesTool/src/ca/demo/salesTool/Main.java
@@ -0,0 +1,17 @@
+package ca.demo.salesTool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData data=new SalesData();
+		displayGreeting();
+		data.display();
+	}
+	public static void 		displayGreeting(){
+		System.out.println("Hello Sales People");
+		System.out.println("Hello App Shows Sales Data");
+	}
+
+
+}
---
+++ SalesTool/src/ca/demo/salesTool/SalesData.java
@@ -0,0 +1,8 @@
+package ca.demo.salesTool;
+
+public class SalesData {
+	int data[]={0,4,42};
+	public void display(){
+		System.out.println("TOOO: implement data dispaly");
+	}
+}
